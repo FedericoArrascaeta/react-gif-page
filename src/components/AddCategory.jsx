63e13262552e5f9045a2184bdd545dcd022e1888
[Federico] <[arrasfede@gmail.com]>
2022-09-04 03:04:14 -0300

    hola

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+export const AddCategory = ({onNewCategory}) => {
+  
+  
+  const [inputValue, setinputValue] = useState('')
+  
+    const onInputChange = ({target}) => {
+      setinputValue(target.value)
+  }
+  
+    const onSubmit = (event) => {
+        event.preventDefault();
+        
+        if (inputValue.trim().length <= 1) return;
+        onNewCategory(inputValue.trim())
+
+        setinputValue('');
+    } 
+  
+    return (
+        <form onSubmit= {onSubmit}>
+            <input
+                type="text"
+                placeholder='buscar gifts'
+                  value={inputValue}
+                  onChange={onInputChange}
+                  
+            />
+        </form>
+  )
+}
